338b1201e46814e7b6f565036023db54b2ad6795
Nuno Facha <nunofacha@gmail.com>
2022-01-07 15:46:37 -0100

    Add README

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
-adsb-logger
+#ADS-B Stats Logger
+
+##Features
+
+- Log all unique flights seen by the ADS-B receiver
+- Log all unique operators seen by the ADS-B receiver
+- Log the flight with the highest altitude seen by the ADS-B receiver
+- Log the flight with the highest speed seen by the ADS-B receiver
+- Log the flight the furthest away from the ADS-B receiver
+- Log the flight closest from the ADS-B receiver
+- Log the flight with the best signal strength seen by the ADS-B receiver
+- Log the flight with the worst signal strength seen by the ADS-B receiver
+- All data is available in Metric Units (by default) or Imperial Units
+
+###WARNING: This is a highly in development script, things can and will change, your data.json file might break and need a manual fix if you update the script to a new version!
+
+## Usage
+1. Clone the git repo on your ADS-B server and enter its directory:
+```bash
+git clone https://github.com/nfacha/adsb-stats-logger.git
+cd adsb-stats-logger
+```
+2. Copy the config file sample:
+```bash
+cp config.ini.sample config.ini
+```
+3. Edit the config to your liking, one easy way would be using nano
+```bash
+nano config.ini
+```
+4. Edit the config to your liking, one easy way would be using nano
+```ini
+[PATHS]
+DATA_PATH = /run/dump1090-fa/ #The path to your dump1090 logs folder
+[LOCATION]
+STATION_LAT = 37.7 #Your base station latitude, used for distance calculations
+STATION_LNG = 37.7 #Your base station longitude, used for distance calculations
+[LOGGING]
+SENTRY_DSN =#Sentry DSN for error logging, leave empry to disable
+LOG_LEVEL = 20 #Log level, see https://docs.python.org/3/library/logging.html#logging-levels
+[UNITS]
+USE_METRIC_SYSTEM = true #Use metric system for distance calculations, else use imperial
+```
+5. Leave the script running in the background, one easy way would be to use screen
+```bash
+screen -S logger -dm python3 logger.py
+```
+6. By now the script should be running, wait a bit for data to gather and as soon as you have at least one plane in your range it shoudl start to gather data, to see this data use
+```bash
+python3 show.py
+```
+You will get an output similar to this:
+
+```
+INFO:root:Logging level: 20
+INFO:root:Use metric system: True
+--- Metrics ---
+Data range: 2022-01-07 16:33:15 - 2022-01-07 13:26:54
+Unique Flights: 5
+Unique Operators: 3
+Max Altitude: Flight IBE6317 11.879000304785126 km at 2022-01-07 13:39:43
+Max Speed: Flight IBE6501 851.3644 kmh at 2022-01-07 13:36:00
+Max Station Distance: Flight RYR28FF 5449.571199579309 km at 2022-01-07 15:34:44
+Min Station Distance: Flight SAT571 12.83198307011012 km at 2022-01-07 16:33:14
+Max Signal: Flight IBE6317 -16.3 db at 2022-01-07 13:39:43
+Min Signal: Flight IBE6501 -24.0 db at 2022-01-07 13:39:43
+--- Unique Flights Seen ---
+SAT2500 last seen on 2022-01-07 13:26:54
+IBE6317 last seen on 2022-01-07 13:39:48
+IBE6501 last seen on 2022-01-07 13:39:48
+RYR28FF last seen on 2022-01-07 15:40:59
+SAT571 last seen on 2022-01-07 16:33:15
+--- Unique Operators Seen ---
+SAT last seen on 2022-01-07 16:33:15
+IBE last seen on 2022-01-07 13:39:48
+RYRFF last seen on 2022-01-07 15:40:59
+
+Process finished with exit code 0
+
+```
+
+If you want to see the runtime logs, or to be able to Ctrl-C you can enter the screen with:
+```bash
+screen -x logger
+```
--- show.py
+++ show.py
@@ -1,20 +1,48 @@
+import configparser
 import json
+import logging
+import sentry_sdk
 
 data = {}
+config = configparser.ConfigParser()
+config.read('config.ini')
+USE_METRIC = True
+
+def load_config():
+    global config
+    global USE_METRIC
+    logging.basicConfig(level=logging.getLevelName(int(config['LOGGING']['LOG_LEVEL'])))
+    logging.info("Logging level: " + config['LOGGING']['LOG_LEVEL'])
+    USE_METRIC = config.getboolean('UNITS', 'USE_METRIC_SYSTEM')
+    logging.info("Use metric system: " + str(USE_METRIC))
+    if config['LOGGING']['SENTRY_DSN'] is not '':
+        sentry_sdk.init(
+            config['LOGGING']['SENTRY_DSN'],
+            traces_sample_rate=1.0,
+            environment=config['LOGGING']['SENTRY_ENVIRONMENT']
+        )
 def load_data():
     global data
     with open('./data.json', 'r') as f:
         data = json.load(f)
 
+load_config()
 load_data()
 print("--- Metrics ---")
 print("Data range: "+data['times']['start'] + " - " + data['times']['latest'])
 print("Unique Flights: "+str(len(data['flights'])))
 print("Unique Operators: "+str(len(data['operators'])))
-print("Max Altitude: Flight "+data['maxAltitude']['flight']+" "+str(data['maxAltitude']['altitude']/3281)+" km at "+data['maxAltitude']['seenAt'])
-print("Max Speed: Flight "+data['maxGroundSpeed']['flight']+" "+str(data['maxGroundSpeed']['groundSpeed']*1.852)+" kmh at "+data['maxGroundSpeed']['seenAt'])
-print("Max Station Distance: Flight "+data['furthestFlight']['flight']+" "+str(data['furthestFlight']['distance']*1.852)+" km at "+data['furthestFlight']['seenAt'])
-print("Min Station Distance: Flight "+data['closestFlight']['flight']+" "+str(data['closestFlight']['distance']*1.852)+" km at "+data['closestFlight']['seenAt'])
+if USE_METRIC:
+    print("Max Altitude: Flight " + data['maxAltitude']['flight'] + " " + str(data['maxAltitude']['altitude'] / 3281) + " km at " + data['maxAltitude']['seenAt'])
+    print("Max Speed: Flight " + data['maxGroundSpeed']['flight'] + " " + str(data['maxGroundSpeed']['groundSpeed'] * 1.852) + " kmh at " + data['maxGroundSpeed']['seenAt'])
+    print("Max Station Distance: Flight " + data['furthestFlight']['flight'] + " " + str(data['furthestFlight']['distance'] * 1.852) + " km at " + data['furthestFlight']['seenAt'])
+    print("Min Station Distance: Flight " + data['closestFlight']['flight'] + " " + str(data['closestFlight']['distance'] * 1.852) + " km at " + data['closestFlight']['seenAt'])
+else:
+    print("Max Altitude: Flight " + data['maxAltitude']['flight'] + " " + str(data['maxAltitude']['altitude']) + " ft at " + data['maxAltitude']['seenAt'])
+    print("Max Speed: Flight " + data['maxGroundSpeed']['flight'] + " " + str(data['maxGroundSpeed']['groundSpeed']) + " kn at " + data['maxGroundSpeed']['seenAt'])
+    print("Max Station Distance: Flight " + data['furthestFlight']['flight'] + " " + str(data['furthestFlight']['distance']) + " nm at " + data['furthestFlight']['seenAt'])
+    print("Min Station Distance: Flight " + data['closestFlight']['flight'] + " " + str(data['closestFlight']['distance']) + " nm at " + data['closestFlight']['seenAt'])
+
 print("Max Signal: Flight "+data['maxSignal']['flight']+" "+str(data['maxSignal']['signal'])+" db at "+data['maxSignal']['seenAt'])
 print("Min Signal: Flight "+data['minSignal']['flight']+" "+str(data['minSignal']['signal'])+" db at "+data['minSignal']['seenAt'])
 print('--- Unique Flights Seen ---')
